390a34b26208f4df22654e442a7af8435fadc87b
刘宝昌 <baochang_liu@sina.com>
2021-10-21 17:50:41 +0800

    #1 PC端加入缩放监听

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,23 @@
 </template>
 
 <style lang="less">
+html, body {
+  background-color: #fff;
+  width: 100vw;
+  height: 100%;
+}
+
 body {
-  margin: 0
+  font-family: PingFang SC, Helvetica Neue, Helvetica, Arial, Hiragino Sans GB, Heiti SC, Microsoft YaHei, WenQuanYi Micro Hei, sans-serif;
+  font-size: 32px;
+  color: #222;
+  margin: 0;
+  padding: 0;
+  overflow-x: hidden;
+  overflow-y: scroll;
+  min-height: 100%;
+  width: 750px;
+  height: 100%;
 }
 
 p, h1, h2, h3, h4, h5 {
@@ -33,11 +48,12 @@ table, tbody, td, th, thead, tr {
   background-color: #fff;
   color: #2c3e50;
   width: 750px;
-  box-shadow: 0 0 3px rgb(0 0 0 / 10%);
+  box-shadow: 0 0 10px rgba(0, 0, 0, 10%);
   min-height: 100%;
   margin: 0 auto;
   position: relative;
   transform-origin: center top;
+  transition: all 0.3s;
 }
 
 #nav {
@@ -63,10 +79,28 @@ table, tbody, td, th, thead, tr {
 }
 </style>
 <script>
-import {defineComponent} from 'vue';
+import {defineComponent, onMounted, onUnmounted} from 'vue';
 import AppNav from "@/components/AppNav";
+import throttle from 'lodash.throttle';
 
 export default defineComponent({
   components: {AppNav},
+  setup() {
+    const handleResize = throttle(() => {
+      const innerWidth = window.innerWidth;
+      if (innerWidth > 750) {
+        document.getElementById("app").style.cssText = `transform: scale(${(750 / innerWidth).toFixed(6)});`
+      } else {
+        document.getElementById("app").style.cssText = "";
+      }
+    }, 50)
+    onMounted(() => {
+      handleResize();
+      window.addEventListener("resize", handleResize);
+    });
+    onUnmounted(() => {
+      window.removeEventListener("resize", handleResize);
+    })
+  },
 });
 </script>
